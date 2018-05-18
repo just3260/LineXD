f1be0c5cc45e2a56c50e1f4f982324f05f6aea3e
”just3260” <just3260@gmail.com>
2018-05-19 01:14:16 +0800

    echo LineBot

--- Package.swift
+++ Package.swift
@@ -4,16 +4,11 @@ import PackageDescription
 let package = Package(
     name: "LineXD",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.4.0"),
+        .package(url: "https://github.com/happiness9721/line-bot-sdk-swift.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        .target(name: "run", dependencies: ["Vapor", "LineBot"]),
     ]
 )
 
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,65 @@
-import App
+import LineBot
+import Foundation
+import Vapor
 
-try app(.detect()).run()
+
+public func randomInt(range:Int) -> Int {
+    #if os(Linux)
+        return Glibc.random() % range
+    #else
+        return Int(arc4random_uniform(UInt32(range)))
+    #endif
+}
+
+
+
+let drop = try Droplet()
+let endpoint = "https://api.line.me/v2/bot/message/reply"
+let accessToken = "OoFdWpqFaiTweCAZ78pVaxcGsNJrzBob0MFrQxHjbmFZmf3Hf1Mr0Z3Rt+CNdWBPHDAPkdCIlLOFfgfPcb22SPqx67yqhD+GBcwWhijCFmwUznCZxhe6Y8cM/HYp/JCyR/7pWcr17f+mab4gBM3ZtgdB04t89/1O/w1cDnyilFU="
+let channelSecret = "496c110eb6db3d1af4918c41647b45ab"
+
+
+drop.post("callback") { request in
+    let bot = LineBot(accessToken: accessToken, channelSecret: channelSecret)
+    
+    guard let content = request.body.bytes?.makeString() else {
+        print("content error")
+        return Response(status: .badRequest)
+    }
+
+    guard let signature = request.headers["X-Line-Signature"] else {
+        print("signature error")
+        return Response(status: .badRequest)
+    }
+
+    guard bot.validateSignature(content: content, signature: signature) else {
+        print("validateSignature error")
+        return Response(status: .badRequest)
+    }
+    
+    guard let events = bot.parseEventsFrom(requestBody: content) else {
+        print("events error")
+        return Response(status: .badRequest)
+    }
+    
+    for event in events {
+        switch event {
+        case .message(let message):
+            let replyToken = message.replyToken
+            switch message.message {
+            case .text(let content):
+                bot.reply(token: replyToken, messages: [.text(text: content.text)])
+                print(content.text)
+            case _:
+                break
+            }
+        case _:
+            break
+        }
+    }
+    
+    return Response(status: .ok, body: "reply")
+}
+
+try drop.run()
+
